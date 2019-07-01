f698ab999919c7805b33de3bb51b4bf492667b08
weijj0528 <weijj0528@163.com>
2019-07-01 16:40:58 +0800

    StatementHandler阅读

--- src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
@@ -30,10 +30,14 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
 /**
+ * 实现 StatementHandler 接口，路由的 StatementHandler 对象，根据 Statement 类型，转发到对应的 StatementHandler 实现类中。
  * @author Clinton Begin
  */
 public class RoutingStatementHandler implements StatementHandler {
 
+  /**
+   * 被委托的 StatementHandler 对象
+   */
   private final StatementHandler delegate;
 
   public RoutingStatementHandler(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
